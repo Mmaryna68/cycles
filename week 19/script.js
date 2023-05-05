fe362dd0cd18f532de59f6c6b89b47311485bfe9
Mmaryna68 <maryov688@gmail.com>
2023-05-05 03:08:28 +0100

    added file and comment in  js

--- week 19/script.js
+++ week 19/script.js
@@ -1,60 +1,74 @@
-/*const taskInput = document.getElementById("taskInput");
-const addTaskButton = document.getElementById("addTaskButton");
-const taskListUl = document.getElementById("taskListUl");
-const clearListButton = document.getElementById("clearListButton");
-const noTaskMessage = document.getElementById("noTaskMessage");
+// Объявляем константы для элементов DOM
+const taskInput = document.getElementById("task-input");
+const addTaskButton = document.getElementById("add-task");
+const taskList = document.getElementById("task-list");
+const clearTasksButton = document.getElementById("clear-tasks");
+const noTaskMessage = document.getElementById("no-tasks-message");
 
-let taskList = [];
+// Функция обновления списка задач
+function updateTaskList() {
+  // Очищаем список задач
+  taskList.innerHTML = "";
 
-function renderTaskList() {
-  taskListUl.innerHTML = "";
-  taskList.forEach((task, index) => {
-    const li = document.createElement("li");
+  // Добавляем задачи из массива tasks в список
+  for (let i = 0; i < tasks.length; i++) {
+    const task = tasks[i];
+    const listItem = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.checked = task.done;
-    checkbox.addEventListener("click", () => {
-      task.done = checkbox.checked;
-      localStorage.setItem("taskList", JSON.stringify(taskList));
+    checkbox.checked = task.isCompleted;
+    // Добавляем обработчик события change, чтобы помечать задачи как выполненные
+    checkbox.addEventListener("change", () => {
+      task.isCompleted = checkbox.checked;
     });
+    listItem.appendChild(checkbox);
     const label = document.createElement("label");
-    label.innerText = task.text;
-    li.appendChild(checkbox);
-    li.appendChild(label);
-    taskListUl.appendChild(li);
-  });
-  if (taskList.length === 0) {
+    label.textContent = task.description;
+    listItem.appendChild(label);
+    taskList.appendChild(listItem);
+  }
+
+  // Если список задач пуст, то отображаем сообщение об отсутствии задач и блокируем кнопку очистки списка
+  if (tasks.length === 0) {
     noTaskMessage.style.display = "block";
-    clearButton.disabled = true;
-  } else {
+    clearTasksButton.disabled = true;
+  }
+  // Если список задач не пуст, то скрываем сообщение и разблокируем кнопку очистки списка
+  else {
     noTaskMessage.style.display = "none";
-    clearButton.disabled = false;
+    clearTasksButton.disabled = false;
   }
 }
 
+// Функция добавления задачи
 function addTask() {
-  const task = taskInput.value;
-  if (task.trim() !== "") {
-    taskList.push({
-      task: task,
-      done: false,
-    });
-    renderTaskList();
+  const taskDescription = taskInput.value;
+  // Проверяем, что описание задачи не пустое
+  if (taskDescription.trim() !== "") {
+    const task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+    // Добавляем новую задачу в массив tasks
+    tasks.push(task);
+    // Обновляем список задач и очищаем поле ввода
+    updateTaskList();
     taskInput.value = "";
   }
 }
 
-function toggleDone(index) {
-  taskList[index].done = !taskList[index].done;
-  renderTaskList();
-}
-
+// Функция очистки списка задач
 function clearTasks() {
-  taskList = [];
-  renderTaskList();
+  // Очищаем массив tasks и обновляем список задач
+  tasks = [];
+  taskInput.value = "";
+  updateTaskList();
 }
 
-addButton.addEventListener("click", addTask);
-clearButton.addEventListener("click", clearTasks);
+// Создаем пустой массив tasks
+let tasks = [];
 
-renderTaskList();
+// Добавляем обработчики событий на кнопки добавления и очистки задач, и вызываем функцию обновления списка задач
+addTaskButton.addEventListener("click", addTask);
+clearTasksButton.addEventListener("click", clearTasks);
+updateTaskList(); // Вызываем функцию обновления списка задач на странице при загрузке страницы
